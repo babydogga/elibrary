7334ee6ed3690e1c08a6650f8e3ee0e67ee743ea
Sylwester Szwedzki <sylwester.szwedzki@microsoft.wsei.edu.pl>
2025-03-10 19:11:40 +0100

    Add project files.

---
+++ elibrary/Controllers/AutorzyController.cs
@@ -0,0 +1,21 @@
+﻿using elibrary.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace elibrary.Controllers
+{
+    public class AutorzyController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AutorzyController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Autorzy.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ elibrary/Models/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace elibrary.Models
+{
+    public class Autor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+        [Display(Name = "Biography")]
+        public string Bio { get; set; }
+
+        //relacje
+
+        public List<Autor_Ksiazka> Autor_Ksiazki { get; set; }
+    }
+}
+
